f9a414a9d6a91749c98a68a2e66a4e415455e858
Nathalie Scherf <nascherf@yahoo.se>
2024-02-05 21:00:53 +0100

    add status of books:button and icon

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-console.log('Hi from my app!')
-
-// console.log(window)
-
-// alert('Hello!')
-// window.alert("with window")
-
-console.log(document);
-
 // Steps: To Add elements:
 
 // 1 Create
@@ -22,22 +13,6 @@ let targetH1 = document.querySelector('h1')
 // add element to target
 targetH1.append(myh2)
 
-// Two other examples: 
-
-// // let userName = prompt('What is your name?')
-// let userName = 'Hubert'
-
-// let pGreeting = document.createElement('p')
-// pGreeting.innerText = `Welcome to the page: ${userName}`
-// let targetA = document.querySelector('a')
-// targetA.append(pGreeting)
-
-// // 
-// let myDiv = document.createElement('div')
-// myDiv.innerHTML = "<h3> This is an h3</h3>"
-// myDiv.style.backgroundColor = 'green'
-// document.querySelector('.collection-list').prepend(myDiv)
-
 
 // Object for book:
 
@@ -47,47 +22,55 @@ let books = [
     authors: 'H. L Dreyfus & S. E Dreyfus',
     year: '1986',
     description: 'a great book about AI',
-    coverImage: 'https://m.media-amazon.com/images/I/71HV7JNWBgL._AC_UF894,1000_QL80_.jpg'
+    coverImage: 'https://m.media-amazon.com/images/I/71HV7JNWBgL._AC_UF894,1000_QL80_.jpg',
+    haveRead: false,
   }, {
     title: 'Neuromancer',
     authors: 'William Gibson',
     year: '1984',
     description: 'a classic cyberpunk novel',
-    coverImage: 'https://m.media-amazon.com/images/I/51rEx2q3+iL._SY346_.jpg'
+    coverImage: 'https://m.media-amazon.com/images/I/51rEx2q3+iL._SY346_.jpg',
+    haveRead: false,
   },
   {
     title: 'The Hitchhiker\'s Guide to the Galaxy',
     authors: 'Douglas Adams',
     year: '1979',
     description: 'a hilarious science fiction classic',
-    coverImage: 'https://m.media-amazon.com/images/I/61ZsRWFIVqL._SY344_BO1,204,203,200_.jpg'
+    coverImage: 'https://m.media-amazon.com/images/I/61ZsRWFIVqL._SY344_BO1,204,203,200_.jpg',
+    haveRead: true,
   },
   {
     title: 'Pride and Prejudice',
     authors: 'Jane Austen',
     year: '1813',
     description: 'a classic novel of manners and love',
-    coverImage: 'https://m.media-amazon.com/images/I/61N8qgZsbYL.jpg'
+    coverImage: 'https://m.media-amazon.com/images/I/61N8qgZsbYL.jpg',
+    haveRead: true,
   },
   {
     title: 'To the Lighthouse',
     authors: 'Virginia Woolf',
     year: '1927',
     description: 'a modernist novel exploring consciousness and perception',
-    coverImage: 'https://m.media-amazon.com/images/I/51pAPu1+fbL.jpg'
+    coverImage: 'https://m.media-amazon.com/images/I/51pAPu1+fbL.jpg',
+    haveRead: false,
+
   }, {
     title: 'The Handmaid\'s Tale',
     authors: 'Margaret Atwood',
     year: '1985',
     description: 'a dystopian novel exploring themes of power and control',
-    coverImage: 'https://m.media-amazon.com/images/I/51ldOMR8vLL.jpg'
+    coverImage: 'https://m.media-amazon.com/images/I/51ldOMR8vLL.jpg',
+    haveRead: true
   },
   {
     title: 'To Kill a Mockingbird',
     authors: 'Harper Lee',
     year: '1960',
     description: 'a powerful novel addressing racial injustice',
-    coverImage: 'https://m.media-amazon.com/images/I/81Oth-+jX4L.jpg'
+    coverImage: 'https://m.media-amazon.com/images/I/81Oth-+jX4L.jpg',
+    haveRead: true
   }
 ]
 
@@ -102,6 +85,8 @@ books.forEach((book) => {
   cardDiv.classList.add('card')
   cardDiv.classList.add('mb-3')
 
+
+
   // add content for the card: 
   cardDiv.innerHTML = `<div class="row g-0">
     <div class="col-md-4">
@@ -112,14 +97,58 @@ books.forEach((book) => {
         <h5 class="card-title">${book.title}</h5>
         <h5>${book.authors}</h5>
         <p class="card-text">${book.description}</p>
-        <p class="card-text  ${ book.year > 1959 ? "bg-info" : "bg-secondary"}"><small class="text-body-secondary">${book.year}</small></p>
+        <div>
+          <i class="bi ${book.haveRead ? "bi-bookmark-check" : "bi-bookmark" } "></i>  
+          <button class="statusButton"> ${ book.haveRead ? 'Have read it' : "Want to read it"} </button>
+        </div>
+        <p class="card-text"><small class="text-body-secondary">${book.year}</small></p>
       </div>
     </div>
   </div>`
- 
   document.querySelector('.collection-list').append(cardDiv)
+})
+
+const changeStatus = (event) => {
+  console.log('status change', event.target);
+   // click on button
+  let buttonWeClickedOn = event.target
+  let icon = buttonWeClickedOn.previousElementSibling
+  //  use the button to find the icon
+  console.log( icon.classList);
+  // change classname of the icon
+  // if the icon has the class -check, remove it, add -bookmark
+  if(icon.classList.contains('bi-bookmark-check')){
+    icon.classList.remove('bi-bookmark-check')
+    icon.classList.add('bi-bookmark')
+    buttonWeClickedOn.innerText = 'Want to read it'
+  } else {
+    icon.classList.remove('bi-bookmark')
+    icon.classList.add('bi-bookmark-check')
+    buttonWeClickedOn.innerText = 'Have read it'
+  }
+
+  console.log(icon);
+  // Alternative: move icon into button, and change inner html of button
+
+ 
+}
+
+
+// Add listner to all status buttons: 
+
+let statusButtons = document.querySelectorAll('.statusButton')
+
+statusButtons.forEach(button => {
+  button.addEventListener('click', changeStatus)
 })
 
 
 
 
+// Using add event listener instead of onclick inline
+
+let buttonWithClick = document.querySelector('#eventButton')
+
+buttonWithClick.addEventListener('click', () => {
+  console.log('Button with event listner added by js');
+})
