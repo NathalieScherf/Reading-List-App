1e10d1d2eee7da1c8856272266938fe5dd3c37e7
Nathalie Scherf <nascherf@yahoo.se>
2024-02-13 10:41:24 +0100

    display books from api

--- script.js
+++ script.js
@@ -38,3 +38,42 @@ let statusButtons = document.querySelectorAll('.statusButton')
 statusButtons.forEach(button => {
   button.addEventListener('click', changeStatus)
 })
+
+
+// Fetch: 
+const getBooks = async () => {
+  let response = await fetch('http://openlibrary.org/subjects/love.json')
+  let data = await response.json()
+  
+  data.works.forEach((book) => {
+  // Build cards using js:
+  let cardDiv = document.createElement('div')
+  // Add styling
+  cardDiv.style.maxWidth = '540px'
+  cardDiv.classList.add('card')
+  cardDiv.classList.add('mb-3')
+  // add content for the card: 
+  cardDiv.innerHTML = `<div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://covers.openlibrary.org/b/id/" + ${book.cover_id} + "-L.jpg" class="img-fluid rounded-start" alt="${book.cover_id}">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${book.title}</h5>
+        <h5>${book.authors[0].name}</h5>
+     
+        <div>
+        
+          <p> ${book.availability.is_readable ? 'You can borrow it' : "You cannot borrow it"} </p>
+        </div>
+        <p class="card-text"><small class="text-body-secondary">${book.first_publish_year}</small></p>
+      </div>
+    </div>
+  </div>`
+  document.querySelector('.collection-list').append(cardDiv)
+})
+
+}
+
+getBooks()
+console.log('After async');
